b4b7b546b7e246f5b46d44c73f06b9293473dee6
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-29 18:12:09 -0300

    feat: :sparkles: Criando controller users e a função create
    Criando o controller de users e a função de create para controlar as resquestes e responses da rota

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,23 @@
+import { Response, Request } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import Jwt, { SignOptions } from 'jsonwebtoken';
+import UserService from '../services/users.service';
+
+const secret = process.env.SECRET as string;
+
+export default class UserController {
+  constructor(private userService = new UserService()) {}
+
+  public create = async (req: Request, res: Response) => {
+    const result = await this.userService.create(req.body);
+
+    const config: SignOptions = {
+      algorithm: 'HS256',
+      expiresIn: '1d',
+    };
+
+    const token = Jwt.sign({ user: result }, secret, config);
+
+    return res.status(StatusCodes.CREATED).json({ token });
+  };
+}
