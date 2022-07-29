4836c087e8397b92f538937063ad35048c3c70cf
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-29 18:13:34 -0300

    feat: :sparkles: Criando a rota POST /users (create)
    Criando a rota POST /users (create) usada para criar um novo usuario no DB

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import ProductsRouters from './routers/products.routes';
+import UsersRouter from './routers/users.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(ProductsRouters);
+app.use(UsersRouter);
 
 export default app;
