12341547acc0b669ca5d85a75d6b8436bb993853
Kleverson Eller <kleverson.a16@gmail.com>
2022-07-29 17:15:46 -0300

    feat: :sparkles: Criando a rota GET /products (getAll)
    Criando a rota responsavel por fazer a requisição de todos os produtos do DB

--- src/routers/products.routes.ts
+++ src/routers/products.routes.ts
@@ -5,6 +5,7 @@ const router = Router();
 
 const productsController = new ProductController();
 
+router.get('/products', productsController.getAll);
 router.post('/products', productsController.create);
 
 export default router;
